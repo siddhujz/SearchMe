32e469674aa0921b09ebdf5fe4e514ad2cc64741
siddhujz <skodali@MCC842.healthcare.uiowa.edu>
2015-11-05 17:51:37 -0600

    first commit

---
+++ SearchMe/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using SearchMe.ViewModel;
+
+namespace SearchMe
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /*protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            SearchMe.MainWindow window = new MainWindow();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }*/
+    }
+}
---
+++ SearchMe/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using SearchMe.ViewModel;
+
+namespace SearchMe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : NavigationWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UserViewModel VM = new UserViewModel();
+            this.DataContext = VM;
+        }
+    }
+}
